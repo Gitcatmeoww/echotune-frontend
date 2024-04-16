95ce59ccf05ec050f513923390c23440f6c5047e
budhraja.ankita <budhraja.ankita@gmail.com>
2024-04-15 21:37:40 -0700

    delete comments

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,18 +4,15 @@ import './LearningGoal/LearningGoalForm.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './Landing/LandingPage';
 import RegistrationForm from './Landing/RegistrationForm';
-// import PreferencePage from './Preference/PreferencePage';
 import PreferencePage from './LearningGoal/PreferencePage';
 import ExplorePage from './Explore/ExplorePage';
 import LearningGoalForm from './LearningGoal/LearningGoalForm';
 import NewsFeed from './FeedPlayer/NewsFeed';
-// import AudioPlayer from './AudioPlayer/AudioPlayer';
 
 const App = () => {
   return (
     <Router>
       <div>
-        {/* Other routes here */}
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/register" element={<RegistrationForm />} />
@@ -23,7 +20,6 @@ const App = () => {
           <Route path="/explore" element={<ExplorePage />} />
           <Route path="/learning-goal" element={<LearningGoalForm />} />
           <Route path="/newsfeed" element={<NewsFeed />} />
-          {/* <Route path="/audio" element={<AudioPlayer />} /> */}
         </Routes>
       </div>
     </Router>
